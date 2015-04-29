a51755b3dd7eab6a4b044ee0547b37ae8b06d473
Darren M <aeneascorrupt@gmail.com>
2015-04-29 01:31:43 -0700

    enable createGame api

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,9 +1,28 @@
 var mainPathfile = '..';
 
+jest.dontMock('q');
 jest.dontMock(mainPathfile);
 describe('Main', function describeMain() {
-  it('should be able to require', function testSanity() {
-    var main = require(mainPathfile);
-    expect(main).toBeTruthy();
+  var main;
+
+  beforeEach(function () {
+    main = require(mainPathfile);
+  });
+
+  afterEach(function () {
+    main = null;
+  });
+
+  pit('should be able to create a new game', function testCreateGame() {
+    var testGameId = 321;
+
+    return main.createGame({
+      gameId: testGameId
+    })
+    .then(function verify(result) {
+      expect(result).toEqual({
+        gameId: 1
+      });
+    });
   });
 });
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = {};
+var q = require('q');
+
+function createGame(params, callback) {
+  return q()
+  .then(function (initialValue) {
+    return {
+      gameId: 1
+    };
+  })
+  .nodeify(callback);
+}
+
+module.exports = {
+  createGame: createGame
+};
