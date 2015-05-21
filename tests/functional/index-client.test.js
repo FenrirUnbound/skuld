ffd999ed40798474e2f499349ee2499d9f780453
Darren M <aeneascorrupt@gmail.com>
2015-05-20 19:56:20 -0700

    temp comit

--- tests/functional/index-client.test.js
+++ tests/functional/index-client.test.js
@@ -13,7 +13,7 @@ describe('Main -- Client', function describeMainClient() {
   });
 
   it('creates a game client', function testSanity(done) {
-    var client = main.createClient(1);
+    var client = main.createClient(1, 2);
     var expectedGameState = {
       gameId: 1,
       players: {},
@@ -27,15 +27,31 @@ describe('Main -- Client', function describeMainClient() {
     expect(client).to.be.an('object');
 
     promise.props({
-      game: client.game
+      game: client.game,
+      self: client.self
     })
     .then(function (result) {
       expect(result.game).to.deep.equal(expectedGameState);
+      expect(result.self).to.deep.equal({
+        id: 2
+      });
     })
     .finally(done);
   });
 
   it('fails when not given a gameId', function failNoGameId() {
     expect(main.createClient).to.throw('No gameId provided to Client constructor');
+  });
+
+  describe('End Turn', function describeEndTurn() {
+    var client;
+
+    beforeEach(function () {
+      client = main.createClient(1);
+    });
+
+    it.skip('it ends the turns', function testEndTurn() {
+      client.endTurn();
+    });
   });
 });
